0f94247f9ae2e433520e28a42c80d8adc13bba6b
Taketomo Oishi <oishi@multisoup.co.jp>
2023-05-18 11:02:13 +0900

    レイヤー部分を別ファイルに切り出し

--- gis/deck.gl/test_deck_map/main.js
+++ gis/deck.gl/test_deck_map/main.js
@@ -1,7 +1,7 @@
 import { Deck, COORDINATE_SYSTEM, OrthographicView } from '@deck.gl/core/typed';
-import { BitmapLayer, ScatterplotLayer, GeoJsonLayer } from '@deck.gl/layers/typed';
-import { readPixelsToArray } from '@luma.gl/core';
+import { renderLayers } from "./RenderLayers";
 
+// 初期カメラ設定
 const INITIAL_VIEW_STATE = {
   target: [1160, 600, 0],
   zoom: 0,
@@ -9,94 +9,17 @@ const INITIAL_VIEW_STATE = {
   maxZoom: 40
 };
 
+// 平行投影設定
 const orthographicView = new OrthographicView({
   flipY: true,
-})
-
-const circles = new ScatterplotLayer({
-  id: "circle1",
-  coordinateSystem: COORDINATE_SYSTEM.CARTESIAN,
-  target: [0, 0, 0],
-  data: [
-    {
-      position: [500, 0],
-      color: [255, 0, 0],
-      radius: 10
-    },
-    {
-      position: [0, 500],
-      color: [255, 255, 0],
-      radius: 20
-    },
-    {
-      position: [0, 0],
-      color: [255, 128, 128],
-      radius: 20
-    },
-    {
-      position: [1800, 0],
-      color: [255, 128, 128],
-      radius: 20
-    },
-    {
-      position: [1800, 1400],
-      color: [115, 255, 255],
-      radius: 30
-    },
-    {
-      position: [0, 1400],
-      color: [115, 255, 255],
-      radius: 30
-    },
-  ],
-  getFillColor: d => d.color,
-  getRadius: d => d.radius
 });
 
-const bitmap_layer1 = new BitmapLayer({
-  id: 'bitmap-layer1',
-  coordinateSystem: COORDINATE_SYSTEM.CARTESIAN,
-  target: [0, 0, 0],
-  bounds: [[0, 1400], [0, 0], [1800, 0], [1800, 1400]],
-  image: 'img/map.png',
-  pickable: true,
-  onClick: ({ bitmap, layer }) => {
-    if (bitmap) {
-      const pixelColor = readPixelsToArray(layer.props.image, {
-        sourceX: bitmap.pixel[0],
-        sourceY: bitmap.pixel[1],
-        sourceWidth: 1,
-        sourceHeight: 1
-      })
-      console.log('Color at picked pixel:', pixelColor);
-      console.log('x: ', bitmap.pixel[0]);
-      console.log('y: ', bitmap.pixel[1]);
-    }
-  }
-});
-
-// ポリゴンデータを出力
-const polygon = new GeoJsonLayer({
-  id: "polygon",
-  coordinateSystem: COORDINATE_SYSTEM.CARTESIAN,
-  target: [0, 0, 0],
-  data: "./data/rooms.json",
-  stroked: true,
-  filled: true,
-  getFillColor: [0, 160, 0, 180],
-  getLineColor: [0, 0, 255, 255],
-  lineWidthMinPixels: 2,
-});
-
+// Deck.glインスタンス生成
 const deckgl = new Deck({
   views: orthographicView,
   coordinateSystem: COORDINATE_SYSTEM.CARTESIAN,
   coordinateOrigin: [0, 0, 0],
   initialViewState: INITIAL_VIEW_STATE,
   controller: true,
-  layers: [
-    bitmap_layer1,
-    circles,
-    polygon
-  ]
+  layers: renderLayers()
 });
