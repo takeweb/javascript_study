3fd40a4f6f9d8598d2fa38f1acb005dd67d95f7d
Taketomo Ohishi <takeweb@mac.com>
2023-08-09 09:17:03 +0900

    make_geojsonを改良

--- make_geojson2/index.js
+++ make_geojson2/index.js
@@ -8,8 +8,8 @@ function make_geojson(x_len, y_len, one_grid) {
     features: [],
   };
 
-  for (let x = 0; x < x_len; x++) {
-    for (let y = 0; y < y_len; y++) {
+  for (let y = 0; y < y_len; y++) {
+    for (let x = 0; x < x_len; x++) {
       let sub_obj = {
         type: "Feature",
         properties: {
--- make_geojson3/index.js
+++ make_geojson3/index.js
@@ -8,15 +8,15 @@ function make_geojson(x_len, y_len, one_grid) {
     features: [],
   };
 
-  for (let x = 0; x < x_len; x++) {
-    for (let y = 0; y < y_len; y++) {
+  for (let y = y_len; y > 0; y--) {
+    for (let x = 0; x < x_len; x++) {
       let sub_obj = {
         type: "Feature",
         properties: {
           grid_no:
             (x + 1).toString().padStart(3, "0") +
             "-" +
-            (y + 1).toString().padStart(3, "0"),
+            (y - y + 1).toString().padStart(3, "0"),
           color: "",
         },
         geometry: {
@@ -25,8 +25,8 @@ function make_geojson(x_len, y_len, one_grid) {
             [
               [x * one_grid, y * one_grid],
               [(x + 1) * one_grid, y * one_grid],
-              [(x + 1) * one_grid, (y + 1) * one_grid],
-              [x * one_grid, (y + 1) * one_grid],
+              [(x + 1) * one_grid, (y - 1) * one_grid],
+              [x * one_grid, (y - 1) * one_grid],
               [x * one_grid, y * one_grid],
             ],
           ],
@@ -35,7 +35,7 @@ function make_geojson(x_len, y_len, one_grid) {
       main_obj.features.push({ ...sub_obj });
     }
   }
-  console.log(JSON.stringify(main_obj, null, 2));
+  // console.log(JSON.stringify(main_obj, null, 2));
   const s = JSON.stringify(main_obj, null, 2);
   try {
     writeFileSync("./grids.json", s, "utf-8");
