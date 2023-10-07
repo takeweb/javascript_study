6f68dab71e5c3b0ddde9615e1ee5fe74db07e88e
Taketomo Oishi <takeweb@mac.com>
2023-10-07 10:08:09 +0900

    2023/10/07学習分まで

--- labo_array/test_map2.js
+++ labo_array/test_map2.js
@@ -1,4 +1,4 @@
-const resultList = [
+const objList = [
   {
     id: 1,
     value: "aaaa",
@@ -14,21 +14,27 @@ const resultList = [
 ];
 
 // オブジェクト配列から、idのみ取り出し
-const ids = resultList.map((x) => {
-  return x.id;
+// コンソール出力結果
+// [ 1, 2, 3 ]
+const ids = objList.map((obj) => {
+  return obj.id;
 });
 console.log(ids);
 
 // オブジェクト配列から、valueのみを取り出し
-const values = resultList.map((x) => x.value);
+// コンソール出力結果
+// [ 'aaaa', 'bbbb', 'cccc' ]
+const values = objList.map((obj) => obj.value);
 console.log(values);
 
 // オブジェクト配列から、idが偶数の要素だけ抽出後加工して出力
-const edited = resultList
-  .filter((v) => {
-    return v.id % 2 === 0;
+// コンソール出力結果
+// [ 'id:2-value:bbbb' ]
+const edited = objList
+  .filter((obj) => {
+    return obj.id % 2 === 0;
   })
-  .map((x) => {
-    return `id:${x.id}-value:${x.value}`;
+  .map((obj) => {
+    return `id:${obj.id}-value:${obj.value}`;
   });
 console.log(edited);
---
+++ test_function.js
@@ -0,0 +1,5 @@
+function incrementNumber(number) {
+    return number + 1
+}
+
+console.log(incrementNumber(123));
